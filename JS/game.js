f19bcaf437fc7260379a9d61820469597920af2e
Yusmen <85336773+YIOsman20@users.noreply.github.com>
2022-03-19 11:34:42 +0200

    Create nextPage function

--- JS/game.js
+++ JS/game.js
@@ -6,3 +6,21 @@ function getAnswer()
     var answer = document.getElementById("answer").value;
 
 }
+function nextPage()
+{
+
+    if(maxHints > 0)
+    {
+        document.getElementById("question").innerHTML = questionNumber;
+        questionNumber++;
+            maxHints -= (2 -hints);
+            hints = 2;
+            document.getElementById("hints").innerHTML = hints;
+            document.getElementById("maxHints").innerHTML = maxHints;
+    }
+    else
+    {
+        document.getElementById("a").innerHTML = "No more hints";
+        document.getElementById("b").innerHTML = " ";
+    }
+}
